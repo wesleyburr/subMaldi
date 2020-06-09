af5047e67d6b8f2c0533bcf1c20c51caed8d97dc
Kristen Yeh <kristenyeh@trentu.ca>
2020-06-09 14:59:45 -0400

    TIC norm added- removed .tar.gz

--- R/normMethod_max.R
+++ R/normMethod_max.R
@@ -11,7 +11,7 @@
 
 
 # Function rescales intensity to 0,1
-normalize <- function(x) {
+.normalize <- function(x) {
   return((x - min(x)) / (max(x) - min(x)))
 }
 
@@ -21,7 +21,7 @@ normalize <- function(x) {
 # ------------------------------
 
 
-normMethod_max <- function(dat, mass_dat, spec1, spec2 = NULL, 
+.normMethod_max <- function(dat, mass_dat, spec1, spec2 = NULL, 
                            spec3 = NULL, spec4 = NULL, spec5 = NULL, 
                            spec6 = NULL){
   
@@ -44,7 +44,7 @@ normMethod_max <- function(dat, mass_dat, spec1, spec2 = NULL,
   
   dat <- select(dat, all_of(mass_dat))
   dat <- transform(dat, "Normalized" = 0)
-  dat["Normalized"] <- normalize(intense)
+  dat["Normalized"] <- .normalize(intense)
   return(dat)
   }
   
@@ -58,8 +58,8 @@ normMethod_max <- function(dat, mass_dat, spec1, spec2 = NULL,
     max_Intensity2 <- max(intense2)
     
     dat <- select(dat, all_of(mass_dat))
-    dat[spec1] <- normalize(intense1)
-    dat[spec2] <- normalize(intense2)
+    dat[spec1] <- .normalize(intense1)
+    dat[spec2] <- .normalize(intense2)
     return(dat)
   }}
   
@@ -75,9 +75,9 @@ normMethod_max <- function(dat, mass_dat, spec1, spec2 = NULL,
     max_Intensity3 <- max(intense3)
     
     dat <- select(dat, all_of(mass_dat))
-    dat[spec1] <- normalize(intense1)
-    dat[spec2] <- normalize(intense2)
-    dat[spec3] <- normalize(intense3)
+    dat[spec1] <- .normalize(intense1)
+    dat[spec2] <- .normalize(intense2)
+    dat[spec3] <- .normalize(intense3)
     return(dat)
   }}
   
@@ -95,10 +95,10 @@ normMethod_max <- function(dat, mass_dat, spec1, spec2 = NULL,
     max_Intensity4 <- max(intense4)
     
     dat <- select(dat, all_of(mass_dat))
-    dat[spec1] <- normalize(intense1)
-    dat[spec2] <- normalize(intense2)
-    dat[spec3] <- normalize(intense3)
-    dat[spec4] <- normalize(intense4)
+    dat[spec1] <- .normalize(intense1)
+    dat[spec2] <- .normalize(intense2)
+    dat[spec3] <- .normalize(intense3)
+    dat[spec4] <- .normalize(intense4)
     return(dat)
   }}
   
@@ -118,11 +118,11 @@ normMethod_max <- function(dat, mass_dat, spec1, spec2 = NULL,
     max_Intensity5 <- max(intense5)
     
     dat <- select(dat, all_of(mass_dat))
-    dat[spec1] <- normalize(intense1)
-    dat[spec2] <- normalize(intense2)
-    dat[spec3] <- normalize(intense3)
-    dat[spec4] <- normalize(intense4)
-    dat[spec5] <- normalize(intense5)
+    dat[spec1] <- .normalize(intense1)
+    dat[spec2] <- .normalize(intense2)
+    dat[spec3] <- .normalize(intense3)
+    dat[spec4] <- .normalize(intense4)
+    dat[spec5] <- .normalize(intense5)
     return(dat)
   }}
   
@@ -144,12 +144,12 @@ normMethod_max <- function(dat, mass_dat, spec1, spec2 = NULL,
     max_Intensity6 <- max(intense6)
     
     dat <- select(dat, all_of(mass_dat))
-    dat[spec1] <- normalize(intense1)
-    dat[spec2] <- normalize(intense2)
-    dat[spec3] <- normalize(intense3)
-    dat[spec4] <- normalize(intense4)
-    dat[spec5] <- normalize(intense5)
-    dat[spec6] <- normalize(intense6)
+    dat[spec1] <- .normalize(intense1)
+    dat[spec2] <- .normalize(intense2)
+    dat[spec3] <- .normalize(intense3)
+    dat[spec4] <- .normalize(intense4)
+    dat[spec5] <- .normalize(intense5)
+    dat[spec6] <- .normalize(intense6)
     return(dat)
   }
 }
@@ -160,11 +160,11 @@ normMethod_max <- function(dat, mass_dat, spec1, spec2 = NULL,
 # ---------------------
 
 # Normalization function specific to custom max_y
-normalize_set <- function(y, max_y){
+.normalize_set <- function(y, max_y){
   return((y - min(y)) / (max_y - min(y)))
 }
 
-normMethod_max_set <- function(dat, mass_dat, spec1, spec2, 
+.normMethod_max_set <- function(dat, mass_dat, spec1, spec2, 
                            spec3 = NULL, spec4 = NULL, spec5 = NULL, 
                            spec6 = NULL){
   
@@ -190,8 +190,8 @@ normMethod_max_set <- function(dat, mass_dat, spec1, spec2,
   max_Intensity2 <- max(intense2)
   
   dat <- select(dat, all_of(mass_dat))
-  dat[spec1] <- normalize_set(y = intense1, max_y = max_int)
-  dat[spec2] <- normalize_set(y = intense2, max_y = max_int)
+  dat[spec1] <- .normalize_set(y = intense1, max_y = max_int)
+  dat[spec2] <- .normalize_set(y = intense2, max_y = max_int)
   return(dat)
   }
   
@@ -214,9 +214,9 @@ normMethod_max_set <- function(dat, mass_dat, spec1, spec2,
     max_Intensity3 <- max(intense3)
     
     dat <- select(dat, all_of(mass_dat))
-    dat[spec1] <- normalize_set(y = intense1, max_y = max_int)
-    dat[spec2] <- normalize_set(y = intense2, max_y = max_int)
-    dat[spec3] <- normalize_set(y = intense3, max_y = max_int)
+    dat[spec1] <- .normalize_set(y = intense1, max_y = max_int)
+    dat[spec2] <- .normalize_set(y = intense2, max_y = max_int)
+    dat[spec3] <- .normalize_set(y = intense3, max_y = max_int)
     return(dat)
   }}
   
@@ -241,10 +241,10 @@ normMethod_max_set <- function(dat, mass_dat, spec1, spec2,
     max_Intensity4 <- max(intense4)
     
     dat <- select(dat, all_of(mass_dat))
-    dat[spec1] <- normalize_set(y = intense1, max_y = max_int)
-    dat[spec2] <- normalize_set(y = intense2, max_y = max_int)
-    dat[spec3] <- normalize_set(y = intense3, max_y = max_int)
-    dat[spec4] <- normalize_set(y = intense4, max_y = max_int)
+    dat[spec1] <- .normalize_set(y = intense1, max_y = max_int)
+    dat[spec2] <- .normalize_set(y = intense2, max_y = max_int)
+    dat[spec3] <- .normalize_set(y = intense3, max_y = max_int)
+    dat[spec4] <- .normalize_set(y = intense4, max_y = max_int)
     return(dat)
   }}
   
@@ -271,11 +271,11 @@ normMethod_max_set <- function(dat, mass_dat, spec1, spec2,
     max_Intensity5 <- max(intense5)
     
     dat <- select(dat, all_of(mass_dat))
-    dat[spec1] <- normalize_set(y = intense1, max_y = max_int)
-    dat[spec2] <- normalize_set(y = intense2, max_y = max_int)
-    dat[spec3] <- normalize_set(y = intense3, max_y = max_int)
-    dat[spec4] <- normalize_set(y = intense4, max_y = max_int)
-    dat[spec5] <- normalize_set(y = intense5, max_y = max_int)
+    dat[spec1] <- .normalize_set(y = intense1, max_y = max_int)
+    dat[spec2] <- .normalize_set(y = intense2, max_y = max_int)
+    dat[spec3] <- .normalize_set(y = intense3, max_y = max_int)
+    dat[spec4] <- .normalize_set(y = intense4, max_y = max_int)
+    dat[spec5] <- .normalize_set(y = intense5, max_y = max_int)
     return(dat)
   }}
   
@@ -305,12 +305,12 @@ normMethod_max_set <- function(dat, mass_dat, spec1, spec2,
     max_Intensity6 <- max(intense6)
     
     dat <- select(dat, all_of(mass_dat))
-    dat[spec1] <- normalize_set(y = intense1, max_y = max_int)
-    dat[spec2] <- normalize_set(y = intense2, max_y = max_int)
-    dat[spec3] <- normalize_set(y = intense3, max_y = max_int)
-    dat[spec4] <- normalize_set(y = intense4, max_y = max_int)
-    dat[spec5] <- normalize_set(y = intense5, max_y = max_int)
-    dat[spec6] <- normalize_set(y = intense6, max_y = max_int)
+    dat[spec1] <- .normalize_set(y = intense1, max_y = max_int)
+    dat[spec2] <- .normalize_set(y = intense2, max_y = max_int)
+    dat[spec3] <- .normalize_set(y = intense3, max_y = max_int)
+    dat[spec4] <- .normalize_set(y = intense4, max_y = max_int)
+    dat[spec5] <- .normalize_set(y = intense5, max_y = max_int)
+    dat[spec6] <- .normalize_set(y = intense6, max_y = max_int)
     return(dat)
   }
 }
