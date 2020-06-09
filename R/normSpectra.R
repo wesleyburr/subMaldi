a1368cb021d326ce0504c1795e5613c22c36cb06
Kristen Yeh <kristenyeh@trentu.ca>
2020-06-09 14:59:45 -0400

    TIC norm added- removed .tar.gz

--- R/normSpectra.R
+++ R/normSpectra.R
@@ -1,5 +1,6 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> 191e6b7... Rendered new documentation using package 'roxygen2'.
 ##' Normalize Spectral Data
@@ -220,10 +221,46 @@ normSpectra <- function(dat, mass_dat, method = NULL, norm_mz = NULL, upper = NU
   
   return(r)
 >>>>>>> 7fb0bb9... Fixed object return bug
+=======
+# -----------------------------------------------------------------------
+# Last Updated: June 8, 2020
+# Author: Kristen Yeh
+# Title: subMALDI: Umbrella Normalization Function
+# -----------------------------------------------------------------------
+
+
+# ------------
+# normSpectra
+# ------------
+
+
+normSpectra <- function(dat, mass_dat, method = NULL, norm_mz = NULL, 
+                        spec1, spec2 = NULL, spec3 = NULL, spec4 = NULL, spec5 = NULL, spec6 = NULL){
+  
+  if(is.null(method)){ stop('Please select a valid normalization method. See ?normSpectra for list of methods.') } 
+  
+  else { 
+    if(method == "max"){ .normMethod_max(dat = dat, mass_dat = mass_dat, spec1 = spec1, spec2 = spec2, 
+                                        spec3 = spec3, spec4 = spec4, spec5 = spec5, spec6 = spec6) }
+  
+    else if(method == "custom"){ .normMethod_custom(dat = dat, mass_dat = mass_dat, norm_mz = norm_mz, spec1 = spec1, spec2 = spec2, 
+                                                   spec3 = spec3, spec4 = spec4, spec5 = spec5, spec6 = spec6) } 
+  
+    else if(method == "max_set"){ 
+       if(is.null(spec2)){ stop('Only one input spectrum. Please input a set of spectra (2 -6).') } 
+        else{ .normMethod_max_set(dat = dat, mass_dat = mass_dat, spec1 = spec1, spec2 = spec2, 
+                                 spec3 = spec3, spec4 = spec4, spec5 = spec5, spec6 = spec6) } }
+    
+    else if( method == "TIC"){
+      .normMethod_TIC(dat = dat, mass_dat = mass_dat, spec1 = spec1, spec2 = spec2, 
+                     spec3 = spec3, spec4 = spec4, spec5 = spec5, spec6 = spec6) }
+  }
+>>>>>>> af5047e... TIC norm added- removed .tar.gz
 }  
 
 
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 # -----------------------------------------------------------------------
@@ -232,4 +269,6 @@ normSpectra <- function(dat, mass_dat, method = NULL, norm_mz = NULL, upper = NU
 =======
 # -----------------------------------------------------------------------
 >>>>>>> 191e6b7... Rendered new documentation using package 'roxygen2'.
+=======
+>>>>>>> af5047e... TIC norm added- removed .tar.gz
 
