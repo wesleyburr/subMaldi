d8f9553f5a8403d6697e137d823e128244bd9861
Kristen Yeh <kristenyeh@trentu.ca>
2020-06-09 14:59:45 -0400

    TIC norm added
    Former-commit-id: 9408390c640624ece8fddf0d78640fb4c83a5d2a
    Former-commit-id: f5df9d5261a4b3347b4644a15f5444d55256a257

--- R/normSpectra.R
+++ R/normSpectra.R
@@ -2,6 +2,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> 191e6b7... Rendered new documentation using package 'roxygen2'.
 =======
@@ -296,10 +297,46 @@ normSpectra <- function(dat, mass_dat, method = NULL, norm_mz = NULL, upper = NU
   
   return(r)
 >>>>>>> 0c7b870... Fixed object return bug
+=======
+# -----------------------------------------------------------------------
+# Last Updated: June 8, 2020
+# Author: Kristen Yeh
+# Title: subMALDI: Umbrella Normalization Function
+# -----------------------------------------------------------------------
+
+
+# ------------
+# normSpectra
+# ------------
+
+
+normSpectra <- function(dat, mass_dat, method = NULL, norm_mz = NULL, 
+                        spec1, spec2 = NULL, spec3 = NULL, spec4 = NULL, spec5 = NULL, spec6 = NULL){
+  
+  if(is.null(method)){ stop('Please select a valid normalization method. See ?normSpectra for list of methods.') } 
+  
+  else { 
+    if(method == "max"){ .normMethod_max(dat = dat, mass_dat = mass_dat, spec1 = spec1, spec2 = spec2, 
+                                        spec3 = spec3, spec4 = spec4, spec5 = spec5, spec6 = spec6) }
+  
+    else if(method == "custom"){ .normMethod_custom(dat = dat, mass_dat = mass_dat, norm_mz = norm_mz, spec1 = spec1, spec2 = spec2, 
+                                                   spec3 = spec3, spec4 = spec4, spec5 = spec5, spec6 = spec6) } 
+  
+    else if(method == "max_set"){ 
+       if(is.null(spec2)){ stop('Only one input spectrum. Please input a set of spectra (2 -6).') } 
+        else{ .normMethod_max_set(dat = dat, mass_dat = mass_dat, spec1 = spec1, spec2 = spec2, 
+                                 spec3 = spec3, spec4 = spec4, spec5 = spec5, spec6 = spec6) } }
+    
+    else if( method == "TIC"){
+      .normMethod_TIC(dat = dat, mass_dat = mass_dat, spec1 = spec1, spec2 = spec2, 
+                     spec3 = spec3, spec4 = spec4, spec5 = spec5, spec6 = spec6) }
+  }
+>>>>>>> f70720d... TIC norm added
 }  
 
 
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -315,4 +352,6 @@ normSpectra <- function(dat, mass_dat, method = NULL, norm_mz = NULL, upper = NU
 =======
 # -----------------------------------------------------------------------
 >>>>>>> ae03a50... Rendered new documentation using package 'roxygen2'.
+=======
+>>>>>>> f70720d... TIC norm added
 
