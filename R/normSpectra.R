20d714fba5f8cb7ba7d7584ce5cce07b6eeecd91
Kristen Yeh <kristenyeh@trentu.ca>
2020-06-12 15:45:50 -0400

    Added new norm methods

--- R/normSpectra.R
+++ R/normSpectra.R
@@ -234,7 +234,7 @@ normSpectra <- function(dat, mass_dat, method = NULL, norm_mz = NULL, upper = NU
 # ------------
 
 
-normSpectra <- function(dat, mass_dat, method = NULL, norm_mz = NULL, 
+normSpectra <- function(dat, mass_dat, method = NULL, norm_mz = NULL, upper = NULL, lower = NULL,
                         spec1, spec2 = NULL, spec3 = NULL, spec4 = NULL, spec5 = NULL, spec6 = NULL){
   
   if(is.null(method)){ stop('Please select a valid normalization method. See ?normSpectra for list of methods.') } 
@@ -267,8 +267,11 @@ normSpectra <- function(dat, mass_dat, method = NULL, norm_mz = NULL,
     else if(method == "median"){
       if(is.null(spec2)){ stop('Only one spectrum input. Please enter two spectra for comparison.') } 
       else{ .normMethod_median(dat = dat, mass_dat = mass_dat, spec1 = spec1, spec2 = spec2, 
-                                spec3 = spec3, spec4 = spec4, spec5 = spec5, spec6 = spec6) }
-    }
+                                spec3 = spec3, spec4 = spec4, spec5 = spec5, spec6 = spec6) } }
+      
+    else if(method == "stdev"){
+      .normMethod_stdev(dat = dat, mass_dat = mass_dat, lower = lower, upper = upper, spec1 = spec1, spec2 = spec2, 
+                                 spec3 = spec3, spec4 = spec4, spec5 = spec5, spec6 = spec6) }
   }
 >>>>>>> af5047e... TIC norm added- removed .tar.gz
 }  
