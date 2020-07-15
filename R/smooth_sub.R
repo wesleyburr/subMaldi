c29c1178ac9800f044731a2b77d105d3d3122b12
Kristen Yeh <kristenyeh@trentu.ca>
2020-07-15 12:42:47 -0400

    Added smoothing

---
+++ R/smooth_sub.R
@@ -0,0 +1,50 @@
+# ----------------------------------------------------------------------------
+# Last Updated: July 8, 2020
+# Author: Kristen Yeh
+# Title: subMALDI - Smoothing: Savitzky-Golay, Kaiser Window, Moving Average
+# ----------------------------------------------------------------------------
+
+
+# ---------------
+# SAVITZKY-GOLAY
+# ---------------
+
+
+.smooth_sg <- function(dat, mass_dat, intensity_dat, p, n, m = 0, ts = 1){
+  
+  intense <- dat[[intensity_dat]]
+  sg <- sgolay(p = p, n = n, m = m, ts = ts)
+  dat$sg <- signal::filter(sg, intense)
+  out <- select(dat, all_of(mass_dat), sg)
+  return(out)
+  
+}
+
+
+# p = filter order
+# n = filter length, must be odd
+# m = return the m-th derivative of the filter coefficients
+# ts = time scaling factor
+
+
+# ---------------
+# ----------------------------------------------------------------------------
+# ---------------
+
+
+# --------------------
+# MOVING AVG. FILTERS
+# --------------------
+
+
+# n = window span
+.smooth_ma <- function(dat, mass_dat, intensity_dat, n){
+  out_i <- stats::filter(intensity_dat, rep(1 / n, n), sides = 2)
+  out<- data.frame(mass_dat, out_i)
+  names(out) <- c("mass", "mov_avg")
+  out <- na.omit(out)
+  return(out)
+}
+
+
+# ----------------------------------------------------------------------------
