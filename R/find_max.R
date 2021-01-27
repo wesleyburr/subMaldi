27e5feae9a6285ce1a243d91f07a442289da6c53
Sophie Castel <sophiecastel@trentu.ca>
2021-01-27 17:17:17 -0500

    JOSS 5 Issue: Generalized functions for any number of spectra; removed redundancies
    Former-commit-id: f13ea02e733751c7eba12d111be0d6092bd28ea6
    Former-commit-id: 0aee1c6859c9eb98ba885a71dc1e9a447702df23

--- R/find_max.R
+++ R/find_max.R
@@ -38,6 +38,7 @@
 # -----------------------------------------------------------------------
 # Last Updated: January 27, 2021
 # Author: Kristen Yeh, Sophie Castel
+<<<<<<< HEAD
 # Title: subMALDI: Find Peak Maxima
 # -----------------------------------------------------------------------
 
@@ -64,16 +65,54 @@ find_max <- function (dat, mass_dat, spectra_cols){
   i <- do.call(what = data.frame, args = c(spectra))
   rownames(i) <- mz
   
+=======
+# Title: subMALDI Peak Maximum Functions
+# -----------------------------------------------------------------------
+
+# -----------------
+# FIND PEAK MAXIMA
+# -----------------
+
+# Find peak maximum and associated m/z of spectra
+find_max <- function (dat, mass_dat, spectra_cols){
+  
+  # --------------
+  # LOGICAL CHECKS
+  # --------------
+  
+  stopifnot(
+    is.character(mass_dat),
+    is.character(spectra_cols),
+    mass_dat %in% colnames(dat),
+    all(spectra_cols %in% colnames(dat))
+  )
+  
+  
+  
+  mz <- dat[[mass_dat]]
+  
+  spectra <- lapply(spectra_cols, function(x){dat[x]})
+  i <- do.call(what = data.frame, args = c(spectra))
+  rownames(i) <- mz
+  
+>>>>>>> 29ad768... JOSS 5 Issue: Generalized functions for any number of spectra; removed redundancies
   max_i <- apply(i, 2, max)
   which_max <- apply(i, 2, which.max)
   max_mz <- mz[which_max]
   
+<<<<<<< HEAD
   max_spec <- data.frame(cbind(max_i, max_mz))
+=======
+  max_spec <- cbind(max_i, max_mz)
+>>>>>>> 29ad768... JOSS 5 Issue: Generalized functions for any number of spectra; removed redundancies
   colnames(max_spec) <- c("Intensity (Max)", "Mass")
   
   return(max_spec)
 }
+<<<<<<< HEAD
 >>>>>>> 5cce370... JOSS 5 Issue: Generalized functions for any number of spectra; removed redundancies
+=======
+>>>>>>> 29ad768... JOSS 5 Issue: Generalized functions for any number of spectra; removed redundancies
 
 # -----------------------------------------------------------------------
 <<<<<<< HEAD
@@ -104,6 +143,7 @@ find_max <- function (dat, mass_dat, spectra_cols){
 # ------------------------
 
 find_max_set <- function(dat, mass_dat, spectra_cols){
+<<<<<<< HEAD
   
   # --------------
   # LOGICAL CHECKS
@@ -148,6 +188,36 @@ find_max_set <- function(dat, mass_dat, spectra_cols){
   return(max_spec)
   
 >>>>>>> 5cce370... JOSS 5 Issue: Generalized functions for any number of spectra; removed redundancies
+=======
+  
+  # --------------
+  # LOGICAL CHECKS
+  # --------------
+  
+  stopifnot(
+    is.character(mass_dat),
+    is.character(spectra_cols),
+    mass_dat %in% colnames(dat),
+    all(spectra_cols %in% colnames(dat))
+  )
+  
+  
+  mz <- dat[[mass_dat]]
+  
+  spectra <- lapply(spectra_cols, function(x){dat[x]})
+  i <- do.call(what = data.frame, args = c(spectra))
+  rownames(i) <- mz
+  
+  which_max <- which(i == max(i), arr.ind = TRUE)
+  
+  max_spec <- data.frame(Intensity = as.numeric(i[which_max[1], which_max[2]]), Mass = as.numeric(rownames(which_max)))
+  
+  colnames(max_spec) <- c("Intensity (Max)", "Mass")
+  rownames(max_spec) <- colnames(i)[which_max[2]]
+  
+  return(max_spec)
+  
+>>>>>>> 29ad768... JOSS 5 Issue: Generalized functions for any number of spectra; removed redundancies
 }
 
 # -----------------------------------------------------------------------
--- R/normMethod_median.R
+++ R/normMethod_median.R
@@ -1,396 +1,65 @@
 # -----------------------------------------------------------------------
-# Last Updated: June 12, 2020
-# Author: Kristen Yeh
+# Last Updated: January 27, 2021
+# Author: Kristen Yeh, Sophie Castel
 # Title: subMALDI: Normalization Method - Median
 # -----------------------------------------------------------------------
-
 
 # ---------------
 # METHOD: MEDIAN
 # ---------------
-
-.normMethod_median <- function(dat, mass_dat, spec1, spec2 = NULL, spec3 = NULL, 
-                              spec4 = NULL, spec5 = NULL, spec6 = NULL){
   
-  if(is.null(spec6)){
-  
-  if(is.null(spec5)){
-  
-  if(is.null(spec4)){
-  
-  if(is.null(spec3)){
-  
-  if(is.null(spec2)){
+.normMethod_median <- function(dat, mass_dat, spectra_cols){
     
-    # One spectrum
-    stop("Only one spectrum input. Please enter two spectra for comparison.")}
-  
-  # ---------------------------------------------------------------------
-  # Two spectra
-  else{
-    intense1 <- dat[[spec1]]
-    intense2 <- dat[[spec2]]
+    # ---------------------
+    # LOGICAL CHECKS
+    # ---------------------
     
-    int <- gather(dat, key = "Spectra", value = "Intensity",
-                  all_of(spec1), all_of(spec2), factor_key = TRUE)
-    int <- dplyr::filter(int, Intensity != 0)
+    stopifnot(
+      is.character(mass_dat),
+      is.character(spectra_cols),
+      mass_dat %in% colnames(dat),
+      all(spectra_cols %in% colnames(dat))
+    )
     
-    s1 <- int[int$Spectra == spec1,] %>% select("full_mz", "Intensity")
-    m1 <- median(s1$Intensity)
+    if(length(spectra_cols) < 2){
+      stop("Only one spectrum input. Please enter two spectra.")
+    }
     
-    s2 <- int[int$Spectra == spec2,] %>% select("full_mz", "Intensity")
-    m2 <- median(s2$Intensity)
     
-    if(m1 > m2){
-      m <- m1/m2
-      dat <- select(dat, all_of(mass_dat))
-      dat[spec1] <- intense1
-      dat[spec2] <- intense2*m
-      return(dat)} 
-    else{
-      m <- m2/m1
-      dat <- select(dat, all_of(mass_dat))
-      dat[spec1] <- intense1*m
-      dat[spec2] <- intense2
-      return(dat)}}}
-  
-  # ---------------------------------------------------------------------
-  # Three spectra
-  else{
-    intense1 <- dat[[spec1]]
-    intense2 <- dat[[spec2]]
-    intense3 <- dat[[spec3]]
+    mz <- dat[[mass_dat]]
     
-    int <- gather(dat, key = "Spectra", value = "Intensity",
-                  all_of(spec1), all_of(spec2), all_of(spec3), factor_key = TRUE)
-    int <- dplyr::filter(int, Intensity != 0)
+    spectra <- lapply(spectra_cols, function(x){dat[x]})
+    i <- do.call(what = data.frame, args = c(spectra))
+    i[i == 0 ] <- NA
     
-    s1 <- int[int$Spectra == spec1,] %>% select("full_mz", "Intensity")
-    s2 <- int[int$Spectra == spec2,] %>% select("full_mz", "Intensity")
-    s3 <- int[int$Spectra == spec3,] %>% select("full_mz", "Intensity")
+    i_med <- apply(i,2, FUN = median, na.rm = TRUE)
+    max_med <- max(i_med)
     
-    m1 <- median(s1$Intensity)
-    m2 <- median(s2$Intensity)
-    m3 <- median(s3$Intensity)
-    m <- c(m1,m2,m3)
-    m_val <- max(m)
-    
-    if(m1 == m_val){
-      a <- m1/m2
-      b <- m1/m3
+    for(j in 1:length(spectra_cols)){
       
-      dat <- select(dat, all_of(mass_dat))
-      dat[spec1] <- intense1
-      dat[spec2] <- intense2*a
-      dat[spec3] <- intense3*b
-      return(dat)} 
-    else if(m2 == m_val){
-      a <- m2/m1
-      b <- m2/m3
+      # multiplicative factors
+      factors <- c()
       
-      dat <- select(dat, all_of(mass_dat))
-      dat[spec1] <- intense1*a
-      dat[spec2] <- intense2
-      dat[spec3] <- intense3*b
-      return(dat)}
-    else{
-      a <- m3/m1
-      b <- m3/m2
-      
-      dat <- select(dat, all_of(mass_dat))
-      dat[spec1] <- intense1*a
-      dat[spec2] <- intense2*b
-      dat[spec3] <- intense3
-      return(dat)}}}
-  
-  # ---------------------------------------------------------------------
-  # Four spectra
-  else{
-    intense1 <- dat[[spec1]]
-    intense2 <- dat[[spec2]]
-    intense3 <- dat[[spec3]]
-    intense4 <- dat[[spec4]]
+      if(i_med[j] == max_med){
+        
+        other <- i_med[-j]
+        other_index <- which(i_med %in% other)
+        
+        factors <- rep(1,length(spectra_cols))
+        
+        factors[other_index] = i_med[j]/i_med[other_index]
+        
+      }
+    }
     
-    int <- gather(dat, key = "Spectra", value = "Intensity",
-                  all_of(spec1), all_of(spec2), all_of(spec3),
-                  all_of(spec4), factor_key = TRUE)
-    int <- dplyr::filter(int, Intensity != 0) 
+    i_mult <- t(t(i)*factors)
     
-    s1 <- int[int$Spectra == spec1,] %>% select("full_mz", "Intensity")
-    s2 <- int[int$Spectra == spec2,] %>% select("full_mz", "Intensity")
-    s3 <- int[int$Spectra == spec3,] %>% select("full_mz", "Intensity")
-    s4 <- int[int$Spectra == spec4,] %>% select("full_mz", "Intensity")
+    dat <- cbind(mz, i_mult)
+    colnames(dat) <- c("full_mz", spectra_cols)
     
-    m1 <- median(s1$Intensity)
-    m2 <- median(s2$Intensity)
-    m3 <- median(s3$Intensity)
-    m4 <- median(s4$Intensity)
-    m <- c(m1,m2,m3,m4)
-    m_val <- max(m)
+    return(dat)
     
-    if(m1 == m_val){
-      a <- m1/m2
-      b <- m1/m3
-      c <- m1/m4
-      
-      dat <- select(dat, all_of(mass_dat))
-      dat[spec1] <- intense1
-      dat[spec2] <- intense2*a
-      dat[spec3] <- intense3*b
-      dat[spec4] <- intense4*c
-      return(dat)} 
-    else if(m2 == m_val){
-      a <- m2/m1
-      b <- m2/m3
-      c <- m2/m4
-      
-      dat <- select(dat, all_of(mass_dat))
-      dat[spec1] <- intense1*a
-      dat[spec2] <- intense2
-      dat[spec3] <- intense3*b
-      dat[spec4] <- intense4*c
-      return(dat)}
-    else if(m3 == m_val){
-      a <- m3/m1
-      b <- m3/m2
-      c <- m3/m4
-      
-      dat <- select(dat, all_of(mass_dat))
-      dat[spec1] <- intense1*a
-      dat[spec2] <- intense2*b
-      dat[spec3] <- intense3
-      dat[spec4] <- intense4*c
-      return(dat)}
-    else{
-      a <- m4/m1
-      b <- m4/m2
-      c <- m4/m3
-      
-      dat <- select(dat, all_of(mass_dat))
-      dat[spec1] <- intense1*a
-      dat[spec2] <- intense2*b
-      dat[spec3] <- intense3*c
-      dat[spec4] <- intense4
-      return(dat)}}}
-  
-  # ---------------------------------------------------------------------
-  # Five spectra
-  else{
-    intense1 <- dat[[spec1]]
-    intense2 <- dat[[spec2]]
-    intense3 <- dat[[spec3]]
-    intense4 <- dat[[spec4]]
-    intense5 <- dat[[spec5]]
-    
-    int <- gather(dat, key = "Spectra", value = "Intensity",
-                  all_of(spec1), all_of(spec2), all_of(spec3),
-                  all_of(spec4), all_of(spec5), factor_key = TRUE)
-    int <- dplyr::filter(int, Intensity != 0) 
-    
-    s1 <- int[int$Spectra == spec1,] %>% select("full_mz", "Intensity")
-    s2 <- int[int$Spectra == spec2,] %>% select("full_mz", "Intensity")
-    s3 <- int[int$Spectra == spec3,] %>% select("full_mz", "Intensity")
-    s4 <- int[int$Spectra == spec4,] %>% select("full_mz", "Intensity")
-    s5 <- int[int$Spectra == spec5,] %>% select("full_mz", "Intensity")
-    
-    m1 <- median(s1$Intensity)
-    m2 <- median(s2$Intensity)
-    m3 <- median(s3$Intensity)
-    m4 <- median(s4$Intensity)
-    m5 <- median(s5$Intensity)
-    m <- c(m1,m2,m3,m4,m5)
-    m_val <- max(m)
-    
-    if(m1 == m_val){
-      a <- m1/m2
-      b <- m1/m3
-      c <- m1/m4
-      d <- m1/m5
-      
-      dat <- select(dat, all_of(mass_dat))
-      dat[spec1] <- intense1
-      dat[spec2] <- intense2*a
-      dat[spec3] <- intense3*b
-      dat[spec4] <- intense4*c
-      dat[spec5] <- intense5*d
-      return(dat)} 
-    else if(m2 == m_val){
-      a <- m2/m1
-      b <- m2/m3
-      c <- m2/m4
-      d <- m2/m5
-      
-      dat <- select(dat, all_of(mass_dat))
-      dat[spec1] <- intense1*a
-      dat[spec2] <- intense2
-      dat[spec3] <- intense3*b
-      dat[spec4] <- intense4*c
-      dat[spec5] <- intense5*d
-      return(dat)}
-    else if(m3 == m_val){
-      a <- m3/m1
-      b <- m3/m2
-      c <- m3/m4
-      d <- m3/m5
-      
-      dat <- select(dat, all_of(mass_dat))
-      dat[spec1] <- intense1*a
-      dat[spec2] <- intense2*b
-      dat[spec3] <- intense3
-      dat[spec4] <- intense4*c
-      dat[spec5] <- intense5*d
-      return(dat)}
-    else if(m4 == m_val){
-      a <- m4/m1
-      b <- m4/m2
-      c <- m4/m3
-      d <- m4/m5
-      
-      dat <- select(dat, all_of(mass_dat))
-      dat[spec1] <- intense1*a
-      dat[spec2] <- intense2*b
-      dat[spec3] <- intense3*c
-      dat[spec4] <- intense4
-      dat[spec5] <- intense5*d
-      return(dat)}
-    else{
-      a <- m5/m1
-      b <- m5/m2
-      c <- m5/m3
-      d <- m5/m4
-      
-      dat <- select(dat, all_of(mass_dat))
-      dat[spec1] <- intense1*a
-      dat[spec2] <- intense2*b
-      dat[spec3] <- intense3*c
-      dat[spec4] <- intense4*d
-      dat[spec5] <- intense5
-      return(dat)}}}
-  
-  # ---------------------------------------------------------------------
-  # Six spectra
-  else{
-    intense1 <- dat[[spec1]]
-    intense2 <- dat[[spec2]]
-    intense3 <- dat[[spec3]]
-    intense4 <- dat[[spec4]]
-    intense5 <- dat[[spec5]]
-    intense6 <- dat[[spec6]]
-    
-    int <- gather(dat, key = "Spectra", value = "Intensity",
-                  all_of(spec1), all_of(spec2), all_of(spec3),
-                  all_of(spec4), all_of(spec5), all_of(spec6),
-                  factor_key = TRUE)
-    int <- dplyr::filter(int, Intensity != 0) 
-    
-    s1 <- int[int$Spectra == spec1,] %>% select("full_mz", "Intensity")
-    s2 <- int[int$Spectra == spec2,] %>% select("full_mz", "Intensity")
-    s3 <- int[int$Spectra == spec3,] %>% select("full_mz", "Intensity")
-    s4 <- int[int$Spectra == spec4,] %>% select("full_mz", "Intensity")
-    s5 <- int[int$Spectra == spec5,] %>% select("full_mz", "Intensity")
-    s6 <- int[int$Spectra == spec6,] %>% select("full_mz", "Intensity")
-    
-    m1 <- median(s1$Intensity)
-    m2 <- median(s2$Intensity)
-    m3 <- median(s3$Intensity)
-    m4 <- median(s4$Intensity)
-    m5 <- median(s5$Intensity)
-    m6 <- median(s6$Intensity)
-    m <- c(m1,m2,m3,m4,m5,m6)
-    m_val <- max(m)
-    
-    if(m1 == m_val){
-      a <- m1/m2
-      b <- m1/m3
-      c <- m1/m4
-      d <- m1/m5
-      e <- m1/m6
-      
-      dat <- select(dat, all_of(mass_dat))
-      dat[spec1] <- intense1
-      dat[spec2] <- intense2*a
-      dat[spec3] <- intense3*b
-      dat[spec4] <- intense4*c
-      dat[spec5] <- intense5*d
-      dat[spec6] <- intense6*e
-      return(dat)} 
-    else if(m2 == m_val){
-      a <- m2/m1
-      b <- m2/m3
-      c <- m2/m4
-      d <- m2/m5
-      e <- m2/m6
-      
-      dat <- select(dat, all_of(mass_dat))
-      dat[spec1] <- intense1*a
-      dat[spec2] <- intense2
-      dat[spec3] <- intense3*b
-      dat[spec4] <- intense4*c
-      dat[spec5] <- intense5*d
-      dat[spec6] <- intense6*e
-      return(dat)}
-    else if(m3 == m_val){
-      a <- m3/m1
-      b <- m3/m2
-      c <- m3/m4
-      d <- m3/m5
-      e <- m3/m6
-      
-      dat <- select(dat, all_of(mass_dat))
-      dat[spec1] <- intense1*a
-      dat[spec2] <- intense2*b
-      dat[spec3] <- intense3
-      dat[spec4] <- intense4*c
-      dat[spec5] <- intense5*d
-      dat[spec6] <- intense6*e
-      return(dat)}
-    else if(m4 == m_val){
-      a <- m4/m1
-      b <- m4/m2
-      c <- m4/m3
-      d <- m4/m5
-      e <- m4/m6
-      
-      dat <- select(dat, all_of(mass_dat))
-      dat[spec1] <- intense1*a
-      dat[spec2] <- intense2*b
-      dat[spec3] <- intense3*c
-      dat[spec4] <- intense4
-      dat[spec5] <- intense5*d
-      dat[spec6] <- intense6*e
-      return(dat)}
-    else if(m5 == m_val){
-      a <- m5/m1
-      b <- m5/m2
-      c <- m5/m3
-      d <- m5/m4
-      e <- m5/m6
-      
-      dat <- select(dat, all_of(mass_dat))
-      dat[spec1] <- intense1*a
-      dat[spec2] <- intense2*b
-      dat[spec3] <- intense3*c
-      dat[spec4] <- intense4*d
-      dat[spec5] <- intense5
-      dat[spec6] <- intense6*e
-      return(dat)}
-    else{
-      a <- m6/m1
-      b <- m6/m2
-      c <- m6/m3
-      d <- m6/m4
-      e <- m6/m5
-      
-      dat <- select(dat, all_of(mass_dat))
-      dat[spec1] <- intense1*a
-      dat[spec2] <- intense2*b
-      dat[spec3] <- intense3*c
-      dat[spec4] <- intense4*d
-      dat[spec5] <- intense5*e
-      dat[spec6] <- intense6
-      return(dat)}
   }
-}
 
 
 
