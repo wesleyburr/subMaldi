2df3f8db314c77a76d3994ec319f2db6d1fd1d1f
Sophie Castel <sophiecastel@trentu.ca>
2021-01-28 13:06:32 -0500

    JOSS 12 (Issue):  Generalized functions for any number of spectra; removed redundancies
    Former-commit-id: 480ae1b82783e4a262a6fd36e078c13c2624350b
    Former-commit-id: ca64c5c0cfdb7a65ceb18279b19be791c586978c

--- R/normMethod_stdev.R
+++ R/normMethod_stdev.R
@@ -1,208 +1,52 @@
 # -----------------------------------------------------------------------
-# Last Updated: June 12, 2020
-# Author: Kristen Yeh
+# Last Updated: January 28, 2021
+# Author: Kristen Yeh, Sophie Castel
 # Title: subMALDI: Normalization Method - Standard Deviation of Noise
 # -----------------------------------------------------------------------
-
 
 # ------------------------------------
 # METHOD: STANDARD DEVIATION OF NOISE
 # ------------------------------------
 
 # Find a region of the spectrum with only noise and minimal peaks
-    # (Should be same region for all spectra)
+# (Should be same region for all spectra)
 # Evaluate standard deviation of intensity in that region for each spec
 # Divide intensity of EACH PEAK IN SPEC by its st. dev.
 
 
-.normMethod_stdev <- function(dat, mass_dat, lower = 900, upper= 1100 , spec1, spec2 = NULL, spec3 = NULL,
-                              spec4 = NULL, spec5 = NULL, spec6 = NULL){
+.normMethod_stdev <- function(dat, mass_dat, lower = 900, upper= 1100 , spectra_cols){
   
-  if(is.null(spec6)){
+  # ---------------------
+  # LOGICAL CHECKS
+  # ---------------------
   
-  if(is.null(spec5)){
+  stopifnot(
+    is.character(mass_dat),
+    is.character(spectra_cols),
+    mass_dat %in% colnames(dat),
+    all(spectra_cols %in% colnames(dat)),
+    is.numeric(lower),
+    is.numeric(upper)
+  )
   
-  if(is.null(spec4)){
+  mz <- dat[[mass_dat]]
   
-  if(is.null(spec3)){
+  spectra <- lapply(spectra_cols, function(x){dat[x]})
+  i <- do.call(what = data.frame, args = c(spectra))
   
-  if(is.null(spec2)){
-    
-    # One spectrum
-    mass <- dat[[mass_dat]]
-    intense <- dat[[spec1]]
-    
-    noise <- intense[which(mass > lower & upper > mass)]
-    noise <- noise[ noise != 0 ]
-    st_dev <- sd(noise)
-    
-    dat <- select(dat, all_of(mass_dat))
-    dat[spec1] <- .norm_TIC(y = intense, all_y = st_dev)
-    return(dat) }
+  noise <- i[which(mz > lower & upper > mz),]
+  noise[noise == 0] <- NA
   
-  # ---------------------------------------------------------------------
-  # Two spectra
-  else{
-    mass <- dat[[mass_dat]]
-    intense1 <- dat[[spec1]]
-    intense2 <- dat[[spec2]]
-    
-    noise1 <- intense1[which(mass > lower & upper > mass)]
-    noise2 <- intense2[which(mass > lower & upper > mass)]
-    
-    noise1 <- noise1[ noise1 != 0 ]
-    noise2 <- noise2[ noise2 != 0 ]
-    
-    st_dev1 <- sd(noise1)
-    st_dev2 <- sd(noise2)
-    
-    dat <- select(dat, all_of(mass_dat))
-    dat[spec1] <- .norm_TIC(y = intense1, all_y = st_dev1)
-    dat[spec2] <- .norm_TIC(y = intense2, all_y = st_dev2)
-    return(dat) } }
+  std_dev <- apply(noise, 2, FUN = sd, na.rm = TRUE)
   
-  # ---------------------------------------------------------------------
-  # Three spectra
-  else{
-    mass <- dat[[mass_dat]]
-    intense1 <- dat[[spec1]]
-    intense2 <- dat[[spec2]]
-    intense3 <- dat[[spec3]]
-    
-    noise1 <- intense1[which(mass > lower & upper > mass)]
-    noise2 <- intense2[which(mass > lower & upper > mass)]
-    noise3 <- intense3[which(mass > lower & upper > mass)]
-    
-    noise1 <- noise1[ noise1 != 0 ]
-    noise2 <- noise2[ noise2 != 0 ]
-    noise3 <- noise3[ noise3 != 0 ]
-    
-    st_dev1 <- sd(noise1)
-    st_dev2 <- sd(noise2)
-    st_dev3 <- sd(noise3)
-    
-    dat <- select(dat, all_of(mass_dat))
-    dat[spec1] <- .norm_TIC(y = intense1, all_y = st_dev1)
-    dat[spec2] <- .norm_TIC(y = intense2, all_y = st_dev2)
-    dat[spec3] <- .norm_TIC(y = intense3, all_y = st_dev3)
-    return(dat) } }
+  i_sd <- t(t(i)*std_dev^-1) # i / std_dev matrix multiplication
   
-  # ---------------------------------------------------------------------
-  # Four spectra
-  else{
-    mass <- dat[[mass_dat]]
-    intense1 <- dat[[spec1]]
-    intense2 <- dat[[spec2]]
-    intense3 <- dat[[spec3]]
-    intense4 <- dat[[spec4]]
-    
-    noise1 <- intense1[which(mass > lower & upper > mass)]
-    noise2 <- intense2[which(mass > lower & upper > mass)]
-    noise3 <- intense3[which(mass > lower & upper > mass)]
-    noise4 <- intense4[which(mass > lower & upper > mass)]
-    
-    noise1 <- noise1[ noise1 != 0 ]
-    noise2 <- noise2[ noise2 != 0 ]
-    noise3 <- noise3[ noise3 != 0 ]
-    noise4 <- noise4[ noise4 != 0 ]
-    
-    st_dev1 <- sd(noise1)
-    st_dev2 <- sd(noise2)
-    st_dev3 <- sd(noise3)
-    st_dev4 <- sd(noise4)
-    
-    dat <- select(dat, all_of(mass_dat))
-    dat[spec1] <- .norm_TIC(y = intense1, all_y = st_dev1)
-    dat[spec2] <- .norm_TIC(y = intense2, all_y = st_dev2)
-    dat[spec3] <- .norm_TIC(y = intense3, all_y = st_dev3)
-    dat[spec4] <- .norm_TIC(y = intense4, all_y = st_dev4)
-    return(dat) } }
-    
-    # ---------------------------------------------------------------------
-    # Five spectra
-    else{
-      mass <- dat[[mass_dat]]
-      intense1 <- dat[[spec1]]
-      intense2 <- dat[[spec2]]
-      intense3 <- dat[[spec3]]
-      intense4 <- dat[[spec4]]
-      intense5 <- dat[[spec5]]
-      
-      noise1 <- intense1[which(mass > lower & upper > mass)]
-      noise2 <- intense2[which(mass > lower & upper > mass)]
-      noise3 <- intense3[which(mass > lower & upper > mass)]
-      noise4 <- intense4[which(mass > lower & upper > mass)]
-      noise5 <- intense5[which(mass > lower & upper > mass)]
-      
-      noise1 <- noise1[ noise1 != 0 ]
-      noise2 <- noise2[ noise2 != 0 ]
-      noise3 <- noise3[ noise3 != 0 ]
-      noise4 <- noise4[ noise4 != 0 ]
-      noise5 <- noise5[ noise5 != 0 ]
-      
-      st_dev1 <- sd(noise1)
-      st_dev2 <- sd(noise2)
-      st_dev3 <- sd(noise3)
-      st_dev4 <- sd(noise4)
-      st_dev5 <- sd(noise5)
-      
-      dat <- select(dat, all_of(mass_dat))
-      dat[spec1] <- .norm_TIC(y = intense1, all_y = st_dev1)
-      dat[spec2] <- .norm_TIC(y = intense2, all_y = st_dev2)
-      dat[spec3] <- .norm_TIC(y = intense3, all_y = st_dev3)
-      dat[spec4] <- .norm_TIC(y = intense4, all_y = st_dev4)
-      dat[spec5] <- .norm_TIC(y = intense5, all_y = st_dev5)
-      return(dat) } }
+  dat <- data.frame(mz, i_sd)
   
-  # ---------------------------------------------------------------------
-  # Six spectra
-  else{
-    mass <- dat[[mass_dat]]
-    intense1 <- dat[[spec1]]
-    intense2 <- dat[[spec2]]
-    intense3 <- dat[[spec3]]
-    intense4 <- dat[[spec4]]
-    intense5 <- dat[[spec5]]
-    intense6 <- dat[[spec6]]
-    
-    noise1 <- intense1[which(mass > lower & upper > mass)]
-    noise2 <- intense2[which(mass > lower & upper > mass)]
-    noise3 <- intense3[which(mass > lower & upper > mass)]
-    noise4 <- intense4[which(mass > lower & upper > mass)]
-    noise5 <- intense5[which(mass > lower & upper > mass)]
-    noise6 <- intense6[which(mass > lower & upper > mass)]
-    
-    noise1 <- noise1[ noise1 != 0 ]
-    noise2 <- noise2[ noise2 != 0 ]
-    noise3 <- noise3[ noise3 != 0 ]
-    noise4 <- noise4[ noise4 != 0 ]
-    noise5 <- noise5[ noise5 != 0 ]
-    noise6 <- noise6[ noise6 != 0 ]
-    
-    st_dev1 <- sd(noise1)
-    st_dev2 <- sd(noise2)
-    st_dev3 <- sd(noise3)
-    st_dev4 <- sd(noise4)
-    st_dev5 <- sd(noise5)
-    st_dev6 <- sd(noise6)
-    
-    dat <- select(dat, all_of(mass_dat))
-    dat[spec1] <- .norm_TIC(y = intense1, all_y = st_dev1)
-    dat[spec2] <- .norm_TIC(y = intense2, all_y = st_dev2)
-    dat[spec3] <- .norm_TIC(y = intense3, all_y = st_dev3)
-    dat[spec4] <- .norm_TIC(y = intense4, all_y = st_dev4)
-    dat[spec5] <- .norm_TIC(y = intense5, all_y = st_dev5)
-    dat[spec6] <- .norm_TIC(y = intense6, all_y = st_dev6)
-    return(dat)
-  }
+  colnames(dat) <- c("full_mz", spectra_cols)
+  
+  return(dat)
 }
-
-
-
-
-
-
-
 
 
 
