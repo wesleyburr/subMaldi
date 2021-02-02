c33300cf05662357e1f60c1033ba9e6229046285
Sophie Castel <sophiecastel@trentu.ca>
2021-02-02 11:35:36 -0500

    Each function in subMALDI now has its own .R file and documentation (if provided previously, except daughter functions).
    Former-commit-id: 6f77461c8e6a9de9b4de197f318a689c005c45f6
    Former-commit-id: 2d197690f3431401d851619c1b7c272cfce08b63

---
+++ R/readcsvSpec.R
@@ -0,0 +1,41 @@
+##' Import a Single .csv Spectrum
+##' 
+##' Imports a single spectrum in .csv format and turns it into a binary data
+##' frame containing a "mass" and "Intensity" column.
+##' 
+##' 
+##' @param spec_file A character string; the file path to the \code{.csv}
+##' spectrum that is to be imported.
+##' @param massCol A character string; the name of the mass column in the
+##' spectrum's \code{.csv} file.
+##' @param intenseCol A character string; the name of the intensity column in
+##' the spectrum's \code{.csv} file.
+##' @return Returns a binary data frame containing the imported data in a
+##' \emph{m/z} column denoted "mass", and an intensity column denoted
+##' "Intensity".
+##' @author Kristen Yeh <kristenyeh@@trentu.ca> Wesley Burr <wburr@@trentu.ca>
+##' @references https://github.com/wesleyburr/subMaldi
+##' @keywords methods
+##' 
+
+# -----------------------------------------------------------------------
+# Last Updated: May 25, 2020
+# Author: Kristen Yeh
+# Title: subMALDI: Import a Single .csv Spectrum
+# -----------------------------------------------------------------------
+
+
+# Import .csv mass spectra files from a directory and export as .rda's for use in data frame
+# Basically just a quick and easy way to turn .csv files into individual data frames per spectrum
+readcsvSpec <- function(spec_file, massCol, intenseCol){
+  temp <- read.csv(file = spec_file)
+  mass <- select(temp, massCol)
+  Intensity <- select(temp, intenseCol)
+  mass <- unlist(cbind(mass))
+  Intensity <- unlist(cbind(Intensity))
+  spec <- data.frame(mass, Intensity)
+  return(spec)
+}
+
+
+# -----------------------------------------------------------------------
