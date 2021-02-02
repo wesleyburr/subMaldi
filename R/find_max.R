ae03a50244c11fd3aa24f96f6a4ad4ed44eaf4d8
Sophie Castel <sophiecastel@trentu.ca>
2021-02-02 11:33:59 -0500

    Rendered new documentation using package 'roxygen2'.

--- R/find_max.R
+++ R/find_max.R
@@ -1,12 +1,38 @@
+##' Find Peak Maxima
+##' 
+##' Analyzes spectral data and returns a list of the most intense peak in each
+##' spectrum, including the \emph{m/z} value associated with the peak.
+##' 
+##' 
+##' @param dat The name of the spectral data frame, containing \code{m/z} data
+##' in the first column and spectral intensity data in subsequent columns.
+##' @param mass_dat A character string; the name of the column in \code{dat}
+##' containing the \emph{m/z} data for the spectrum.
+##' @param spectra_cols A character vector; the names of the column in
+##' \code{dat} containing the intensity data for the spectra-of-interest.
+##' @return Returns a data frame indidcating the most intense peaks of each
+##' input spectrum. Indicates the spectrum the data is from, the \emph{m/z}
+##' value associated with the peak, and the intensity of the maxima.
+##' @author Kristen Yeh <kristenyeh@@trentu.ca> Wesley Burr <wburr@@trentu.ca>
+##' Sophie Castel <sophie.castel@@ontariotechu.net>
+##' @references https://github.com/wesleyburr/subMaldi
+##' @keywords methods
+##' @examples
+##' 
+##' ## Load sample dataset "Master.rda"
+##' data("Master")
+##' 
+##' 
+##' ## Find maxima of four spectra
+##' find_max(dat = Master, mass_dat = "full_mz", spectra_cols = c("Blank1", "Before1", "After1", "After2")
+##'
+
 # -----------------------------------------------------------------------
 # Last Updated: January 27, 2021
 # Author: Kristen Yeh, Sophie Castel
-# Title: subMALDI Peak Maximum Functions
+# Title: subMALDI: Find Peak Maxima
 # -----------------------------------------------------------------------
 
-# -----------------
-# FIND PEAK MAXIMA
-# -----------------
 
 # Find peak maximum and associated m/z of spectra
 find_max <- function (dat, mass_dat, spectra_cols){
@@ -41,47 +67,3 @@ find_max <- function (dat, mass_dat, spectra_cols){
 }
 
 # -----------------------------------------------------------------------
-
-
-# ------------------------
-# FIND PEAK MAXIMA OF SET
-# ------------------------
-
-find_max_set <- function(dat, mass_dat, spectra_cols){
-  
-  # --------------
-  # LOGICAL CHECKS
-  # --------------
-  
-  stopifnot(
-    is.character(mass_dat),
-    is.character(spectra_cols),
-    mass_dat %in% colnames(dat),
-    all(spectra_cols %in% colnames(dat))
-  )
-  
-  if(length(spectra_cols) < 2){
-    stop("Only one spectrum input. Please enter two spectra.")
-  }
-  
-  
-  mz <- dat[[mass_dat]]
-  
-  spectra <- lapply(spectra_cols, function(x){dat[x]})
-  i <- do.call(what = data.frame, args = c(spectra))
-  rownames(i) <- mz
-  
-  which_max <- which(i == max(i), arr.ind = TRUE)
-  
-  max_spec <- data.frame(Intensity = as.numeric(i[which_max[1], which_max[2]]), Mass = as.numeric(rownames(which_max)))
-  
-  colnames(max_spec) <- c("Intensity (Max)", "Mass")
-  rownames(max_spec) <- colnames(i)[which_max[2]]
-  
-  return(max_spec)
-  
-}
-
-
-# -----------------------------------------------------------------------
-
--- R/smoothSpectrum.R
+++ R/smoothSpectrum.R
@@ -1,7 +1,56 @@
+##' Smooth Noise in Spectrum
+##' 
+##' Offers two different methods for smoothing noise in raw spectral data: a
+##' moving average filter and the Savitzky-Golay filter (1).
+##' 
+##' 
+##' @param dat The name of the spectral data frame, containing \code{m/z} data
+##' in the first column and spectral intensity data in subsequent columns.
+##' @param mass_dat A character string; the name of the column in \code{dat}
+##' containing the \emph{m/z} data for the spectrum.
+##' @param intensity_dat A character string; the name of the column in
+##' \code{dat} containing the intensity data for the spectrum to be smoothed.
+##' @param method A character string; the method to be used for smoothing.
+##' Available methods include a Savitzky-Golay filter (\code{"sgolay"}) and a
+##' moving average filter (\code{"mov_avg."})
+##' @param p Single numeric value. If \code{method = "sgolay"}, the filter
+##' order of smoothing. Default = NULL.
+##' @param n Single odd numeric value. If \code{method = "sgolay"}, the length
+##' of the smoothing filter. If \code{method = "mov_avg"}, the window span
+##' size. Default = NULL.
+##' @param m Single numeric value. If \code{method = "sgolay"}, returns the
+##' m-th derivative of the filter coefficients. Default = 0.
+##' @param ts Single numeric value. If \code{method = "sgolay"}, the time
+##' scaling factor. Default = 1.
+##' @return Returns a new data frame containing the smoothed spectral data.
+##' @author Kristen Yeh <kristenyeh@@trentu.ca> Wesley Burr <wburr@@trentu.ca>
+##' @references https://github.com/wesleyburr/subMaldi (1) A. Savitzky, M.J.E.
+##' Golay, Smoothing and differentiation of data by simplified least-squares
+##' procedures, Anal. Chem. 36 (8) (1964) 1627-1639.
+##' @keywords methods manip
+##' @examples
+##' 
+##' ## Load sample dataset "Before1.rda"
+##' data("Before1")
+##' 
+##' ## Testing method "sgolay"
+##' test <- smoothSpectrum(dat = Before1, mass_dat = "mass",
+##'                         intensity_dat = "Intensity", 
+##'                         method = "sgolay", p = 4, 
+##'                         n = 25, m = 0)
+##' 
+##' ## Testing method "mov_avg"                        
+##' test <- smoothSpectrum(dat = Before1, mass_dat = Before1$mass,
+##'                         intensity_dat = Before1$Intensity,
+##'                         method = "mov_avg", n = 25)
+##'                    
+##' 
+
+
 # ----------------------------------------------------------------------------
 # Last Updated: July 10, 2020
 # Author: Kristen Yeh
-# Title: subMALDI - Smoothing Parent Function
+# Title: subMALDI: Smooth Noise in Spectrum
 # ----------------------------------------------------------------------------
 
 
