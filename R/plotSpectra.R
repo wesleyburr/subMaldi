c588c0a85cdfd85c381c9025df360c7f18a1bd1a
Sophie Castel <sophiecastel@trentu.ca>
2021-02-03 11:04:12 -0500

    Combined legacy plotting functions (plotSpectrum(), plotSpectra(), plotgridSpectra()) into a single function; plotSpectra(). Included parameter 'nrows' to allow user to construct a grid layout.
    Former-commit-id: 3eeaddfe53a0c247cd057df83610fa07116293f7
    Former-commit-id: bccab8510113119068dbc34b55b3c7a45d8efc4c

--- R/plotSpectra.R
+++ R/plotSpectra.R
@@ -1,42 +1,19 @@
-##' Plot and Compare Multiple Spectra
+##' Plot and Compare Spectra
 ##' 
-##' Plots multiple spectra on the same \emph{x}-axis scale for easier
-##' comparison. Allows for 2 to 6 spectra per plot, stacked on top of one
-##' another.
+##' Plot single or multiple spectra. For multiple spectra, a grid layout can be called using 'nrows'.
 ##' 
 ##' 
 ##' @param dat The name of the spectral data frame, containing the \emph{m/z}
 ##' data in the first column.
 ##' @param mass_dat A character string; the name of the column in \code{dat}
 ##' containing the \emph{m/z} data.
-##' @param spec1 A character string; the name of the column in \code{dat}
-##' containing the intensity data for the first spectrum that is to be plotted.
-##' @param spec2 A character string; the name of the column in \code{dat}
-##' containing the intensity data for the second spectrum to be plotted.
-##' @param spec3 A character string; the name of the column in \code{dat}
-##' containing the intensity data for the third spectrum to be plotted.
-##' @param spec4 A character string; the name of the column in \code{dat}
-##' containing the intensity data for the fourth spectrum to be plotted.
-##' @param spec5 A character string; the name of the column in \code{dat}
-##' containing the intensity data for the fifth spectrum to be plotted.
-##' @param spec6 A character string; the name of the column in \code{dat}
-##' containing the intensity data for the sixth spectrum to be plotted.
-##' @param colour1 A character string indicating the colour the first spectrum
-##' should be plotted in.
-##' @param colour2 A charater string indicating the colour of the second
-##' spectrum in the plot.
-##' @param colour3 A charater string indicating the colour of the third
-##' spectrum in the plot.
-##' @param colour4 A charater string indicating the colour of the fourth
-##' spectrum in the plot.
-##' @param colour5 A charater string indicating the colour of the fifth
-##' spectrum in the plot.
-##' @param colour6 A charater string indicating the colour of the sixth
-##' spectrum in the plot.
-##' @param span Single numeric value; the span of peak maximas in between each
+##' @param spectra_cols A character string; the name(s) of the column(s) in \code{dat}
+##' containing the intensity data for the spectra-of-interest.
+##' @param colours A character string indicating the desired colour(s)
+##' @param span Single numeric value; the span of peak maxima in between each
 ##' label. Default = 5 (ignores two peak maxima on either side of each label).
 ##' @param thresh Single numeric value (0-100); the threshold of peak
-##' itensities which should be labelled.
+##' intensities which should be labeled.
 ##' @param lbls Logical. If \code{lbls = TRUE}, labels indicating the
 ##' \emph{m/z} value of each peak maxima within the indicated \code{span} will
 ##' be included in the output plot. If \code{lbls = FALSE}, labels are not
@@ -53,14 +30,15 @@
 ##' range.
 ##' @param max_I Single numeric value; upper end of the intensity range
 ##' observed in spectra.
-##' @param x_ticks Single numeric value; the space between ticks on x-axis.
+##' @param x_ticks Single numeric value; the number of ticks on the x-axis.
+##' @param nrows Single integer value; the number of rows in plot arrangement.
 ##' @param intensity_scale A character string; the method that should be used
 ##' for determining the y-axis scales for each spectrum. If \code{method =
 ##' "free_y"}, each spectrum will be plotted with its own intensity scale. If
 ##' \code{method = "fixed"}, each spectrum will be plotted with the y-axis of
 ##' the most intense spectrum in the set.
 ##' @return Returns a line plot of the input spectra.
-##' @author Kristen Yeh <kristenyeh@@trentu.ca> Wesley Burr <wburr@@trentu.ca>
+##' @author Kristen Yeh <kristenyeh@@trentu.ca> Wesley Burr <wburr@@trentu.ca> Sophie Castel <sophie.castel@@ontariotechu.net>
 ##' @references https://github.com/wesleyburr/subMaldi
 ##' @keywords methods aplot
 ##' @examples
@@ -68,228 +46,143 @@
 ##' ## Plotting using the sample dataset "Master.rda"
 ##' data("Master")
 ##' plotSpectra(dat = Master, mass_dat = "full_mz",
-##'             spec1 = "Blank1", spec2 = "Blank2",
-##'             intensity_scale = "free_y", lbls = TRUE)
+##'             spectra_cols = c("Blank1", "Blank2"),
+##'             intensity_scale = "free_y", lbls = TRUE, nrows = 2, x_ticks = 10)
 
 # -----------------------------------------------------------------------
-# Last Updated: July 15, 2020
-# Author: Kristen Yeh
+# Last Updated: February 3, 2021
+# Author: Kristen Yeh, Sophie Castel
 # Title: subMALDI: Plot and Compare Multiple Spectra
 # -----------------------------------------------------------------------
 
 plotSpectra <- function(dat, mass_dat,
-                        spec1, spec2 = NULL,
-                        spec3 = NULL,
-                        spec4 = NULL,
-                        spec5 = NULL,
-                        spec6 = NULL,
-                        colour1 = "steelblue1",
-                        colour2 = "lightpink1",
-                        colour3 = "coral",
-                        colour4 = "goldenrod1",
-                        colour5 = "gold",
-                        colour6 = "seagreen3",
+                        spectra_cols, 
+                        colours = brewer.pal(length(spectra_cols), "Set2"),
                         span = 5,
                         thresh = 0.1,
                         lbls = FALSE, 
                         lbl.fmt = "%3.4f",
                         min_mz = 0,
-                        max_mz = 10000,
+                        max_mz = max(dat[[mass_dat]]),
                         min_I = 0,
-                        max_I = NULL,
+                        max_I = max(dat[spectra_cols]),
                         x_ticks = 100,
+                        nrows = round_any(length(spectra_cols), 2, f = ceiling)/2,
                         intensity_scale = "free_y"){   
-  options(warn=-1)
+  
+  
+  # Functions to check that labels have less than or equal to decimal places as data
+  
+  .deci <- function(x) {
+    if ((x %% 1) != 0) {
+      nchar(strsplit(sub('0+$', '', as.character(x)), ".", fixed=TRUE)[[1]][[2]])
+    } else {
+      return(0)
+    }
+  }
+  
+  .test_lbl <- function(mass_dat, lbl.fmt){
+    x <- mass_dat
+    dp <- c()
+    out <- c()
+    
+    lbl.dec <- strsplit(lbl.fmt, "[.]")[[1]][2]
+    lbl.dec <- as.numeric(gsub("[a-zA-Z ]", "", lbl.dec))
+    
+    for(i in 1:length(x)){
+      dp[i] <- .deci(x[i])
+    }
+    
+    dp <- max(dp)
+    out <- isTRUE(dp < lbl.dec) 
+    return(out)
+  }
+  
+  # ----------------------------------
+  # LOGICAL CHECKS
+  # ----------------------------------
+  
+  
+  stopifnot(spectra_cols %in% colnames(dat),
+            mass_dat %in% colnames(dat),
+            is.character(colours),
+            length(colours) == length(spectra_cols),
+            intensity_scale == "free_x" | intensity_scale == "free_y" | intensity_scale == "free",
+            all(is.numeric(nrows), 
+                is.numeric(min_mz), 
+                is.numeric(max_mz), 
+                is.numeric(min_I), 
+                is.numeric(max_I), 
+                is.numeric(x_ticks),
+                is.numeric(span),
+                is.numeric(thresh))
+            )
+  
+  
   mass <- dat[[mass_dat]]
   
-  if(min_mz < min(mass_dat)){
-    min_mz <- floor(min(mass)) }
+  if(min_mz < min(mass)){
+    warning("Specified value of 'min_mz' is beyond the mass range in 'dat'. Defaulting to the minimum mass.")
+    min_mz <- floor(min(mass)) 
+    }
   
-  if(max_mz > max(mass_dat)){
-    max_mz <- ceiling(max(mass)) }
-  
-  spec <- list(spec1, spec2, spec3, spec4, spec5, spec6)
-  nulls <- c()
-  
-  for(i in 1:length(spec)){
-    if(is.null(spec[[i]])){
-      null <- i
-      nulls <- append(nulls, null, after = length(nulls)) } 
+  if(max_mz > max(mass)){
+    warning("Specified value of 'max_mz' is beyond the mass range in 'dat'. Defaulting to the maximum mass.")
+    max_mz <- ceiling(max(mass)) 
   }
   
-  n <- length(spec)-length(nulls)
+
   
-  if(n == 1){
-    stop('Only one input spectrum. Please use plotSpectrum().')
-  }
   
-  if(n == 2){
-    # ensure spec names are columns in dat
-    stopifnot(is.character(spec1), is.character(spec2),
-              spec1 %in% names(dat), spec2 %in% names(dat))
-    
-    names(dat) <- c("full_mz", spec1, spec2)
-    
-    # reformat the data frame for easy faceting
-    sorted <- gather(dat, key = "Spectra", value = "Intensity", 
-                     all_of(spec1), all_of(spec2), 
-                     factor_key= TRUE) }
+  spectra <- lapply(X = spectra_cols, FUN = function(x){ dat[x] })
   
-  if(n == 3){
-    # ensure spec names are columns in dat
-    stopifnot(is.character(spec1), is.character(spec2),
-              is.character(spec3), spec1 %in% names(dat), 
-              spec2 %in% names(dat), spec3 %in% names(dat))
-    
-    names(dat) <- c("full_mz", spec1, spec2, spec3)
-    
-    # reformat the data frame for easy faceting
-    sorted <- gather(dat, key = "Spectra", value = "Intensity", 
-                     all_of(spec1), all_of(spec2), all_of(spec3),
-                     factor_key= TRUE) }
+  i <- do.call(what = data.frame, args = c(spectra))
   
-  if(n == 4){
-    # ensure spec names are columns in dat
-    stopifnot(is.character(spec1), is.character(spec2),
-              is.character(spec3), is.character(spec4),
-              spec1 %in% names(dat), spec2 %in% names(dat), 
-              spec3 %in% names(dat), spec4 %in% names(dat))
-    
-    names(dat) <- c("full_mz", spec1, spec2, spec3, spec4)
-    
-    # reformat the data frame for easy faceting
-    sorted <- gather(dat, key = "Spectra", value = "Intensity", 
-                     all_of(spec1), all_of(spec2), all_of(spec3),
-                     all_of(spec4), factor_key= TRUE) }
+  dat <- data.frame(full_mz = mass, i)
+  colnames(dat) <- c("full_mz", spectra_cols)
   
-  if(n == 5){
-    # ensure spec names are columns in dat
-    stopifnot(is.character(spec1), is.character(spec2),
-              is.character(spec3), is.character(spec4),
-              is.character(spec5), spec1 %in% names(dat), 
-              spec2 %in% names(dat), spec3 %in% names(dat), 
-              spec4 %in% names(dat), spec5 %in% names(dat))
-    
-    names(dat) <- c("full_mz", spec1, spec2, spec3, spec4,
-                    spec5)
-    
-    # reformat the data frame for easy faceting
-    sorted <- gather(dat, key = "Spectra", value = "Intensity", 
-                     all_of(spec1), all_of(spec2), all_of(spec3),
-                     all_of(spec4), all_of(spec5), factor_key= TRUE) }
+  dat_melt <- melt(dat, id.vars = "full_mz")
+  colnames(dat_melt) <- c("full_mz","Spectrum","Intensity")
   
-  if(n == 6){
-    # ensure spec names are columns in dat
-    stopifnot(is.character(spec1), is.character(spec2),
-              is.character(spec3), is.character(spec4),
-              is.character(spec5), is.character(spec6),
-              spec1 %in% names(dat), spec2 %in% names(dat), 
-              spec3 %in% names(dat), spec4 %in% names(dat), 
-              spec5 %in% names(dat), spec6 %in% names(dat))
-    
-    names(dat) <- c("full_mz", spec1, spec2, spec3, spec4,
-                    spec5, spec6)
-    
-    # reformat the data frame for easy faceting
-    sorted <- gather(dat, key = "Spectra", value = "Intensity", 
-                     all_of(spec1), all_of(spec2), all_of(spec3),
-                     all_of(spec4), all_of(spec5), all_of(spec6),
-                     factor_key= TRUE) }
-  
-  # If lbls = TRUE
-  if(lbls == TRUE & is.null(max_I)){
-    tst <- .test_lbl(mass_dat = mass, lbl.fmt = lbl.fmt)
-    # Check that labels have <= decimal places as data
-    if(tst == TRUE){
-      stop('Label format indicates more decimal places 
-           than given in data.') 
-    } else{
-      # plot the sorted data
-      ggplot(sorted, 
-             aes(x = full_mz, y = Intensity, colour = Spectra)) +
-        geom_line() +
+  p <- ggplot(data = dat_melt) + geom_line(aes(x = full_mz, y = Intensity, colour = Spectrum)) + 
+
         labs(x = expression(italic("m/z")), y = "Intensity") +
-        facet_wrap(~Spectra, ncol = 1, scales = intensity_scale) +
+
+        facet_wrap(~Spectrum, nrow = nrows, scales = intensity_scale) +
+
         scale_x_continuous(limits = c(min_mz, max_mz), 
                            breaks = seq(min_mz, max_mz, 
-                                        by = x_ticks)) +
-        theme_bw() + theme(panel.border = element_blank(),
-                           strip.background = element_blank(),
-                           strip.text.x = element_blank(),
-                           legend.position = "bottom",
-                           axis.line = element_line(colour = "grey85",
-                                                    size = 0.5)) +
-        scale_color_manual(values = c(colour1,colour2, colour3,
-                                      colour4,colour5,colour6)) +
-        stat_peaks(aes(x = full_mz, y = Intensity, group = 1),
-                   ignore_threshold = thresh, span = span,
-                   x.label.fmt = lbl.fmt, geom = "text",
-                   check_overlap = TRUE, color = "black", cex = 3.0) }
+                                    length.out = x_ticks)) +
     
-  } else if(lbls == FALSE & is.null(max_I)){
-    # plot the sorted data
-    ggplot(sorted, aes(x = full_mz, y = Intensity, 
-                       colour = Spectra)) +
-      geom_line() +
-      labs(x = expression(italic("m/z")), y = "Intensity") +
-      facet_wrap(~Spectra, ncol = 1, scales = intensity_scale) +
-      scale_x_continuous(limits = c(min_mz, max_mz), 
-                         breaks = seq(min_mz, max_mz, by = x_ticks)) +
-      theme_bw() + theme(panel.border = element_blank(),
-                         strip.background = element_blank(),
-                         strip.text.x = element_blank(),
-                         legend.position = "bottom",
-                         axis.line = element_line(colour = "grey85", 
-                                                  size = 0.5)) +
-      scale_color_manual(values = c(colour1,colour2,colour3,
-                                    colour4,colour5,colour6)) 
-  } else if(lbls == TRUE){
-    tst <- .test_lbl(mass_dat = mass, lbl.fmt = lbl.fmt)
-    # Check that labels have <= decimal places as data
-    if(tst == TRUE){
-      stop('Label format indicates more decimal places 
-           than given in data.') 
-    } else{
-      # plot the sorted data
-      ggplot(sorted, 
-             aes(x = full_mz, y = Intensity, colour = Spectra)) +
-        geom_line() +
-        labs(x = expression(italic("m/z")), y = "Intensity") +
-        facet_wrap(~Spectra, ncol = 1, scales = intensity_scale) +
-        scale_x_continuous(limits = c(min_mz, max_mz), 
-                           breaks = seq(min_mz, max_mz, 
-                                        by = x_ticks)) +
         scale_y_continuous(limits = c(min_I, max_I)) +
+
         theme_bw() + theme(panel.border = element_blank(),
                            strip.background = element_blank(),
                            strip.text.x = element_blank(),
                            legend.position = "bottom",
                            axis.line = element_line(colour = "grey85",
                                                     size = 0.5)) +
-        scale_color_manual(values = c(colour1,colour2, colour3,
-                                      colour4,colour5,colour6)) +
-        stat_peaks(aes(x = full_mz, y = Intensity, group = 1),
-                   ignore_threshold = thresh, span = span,
-                   x.label.fmt = lbl.fmt, geom = "text",
-                   check_overlap = TRUE, color = "black", cex = 3.0) }
-  } else if(lbls == FALSE){
-    ggplot(sorted, aes(x = full_mz, y = Intensity, 
-                       colour = Spectra)) +
-      geom_line() +
-      labs(x = expression(italic("m/z")), y = "Intensity") +
-      facet_wrap(~Spectra, ncol = 1, scales = intensity_scale) +
-      scale_x_continuous(limits = c(min_mz, max_mz), 
-                         breaks = seq(min_mz, max_mz, by = x_ticks)) +
-      scale_y_continuous(limits = c(min_I, max_I)) +
-      theme_bw() + theme(panel.border = element_blank(),
-                         strip.background = element_blank(),
-                         strip.text.x = element_blank(),
-                         legend.position = "bottom",
-                         axis.line = element_line(colour = "grey85", 
-                                                  size = 0.5)) +
-      scale_color_manual(values = c(colour1,colour2,colour3,
-                                    colour4,colour5,colour6))
+
+        scale_color_manual(values = colours)
+  
+  if(lbls){
+    
+    # Check that labels have <= decimal places as data
+    tst <- .test_lbl(mass_dat = mass, lbl.fmt = lbl.fmt)
+    
+    if(tst){
+      stop("Label format indicates more decimal places 
+           than given in data.")
+    }
+    
+    p <- p + stat_peaks(aes(x = full_mz, y = Intensity, group = 1),
+                        ignore_threshold = thresh, span = span,
+                        x.label.fmt = lbl.fmt, geom = "text",
+                        check_overlap = TRUE, color = "black", cex = 3.0)
   }
+  
+  return(p)
+  
 }
 
 
