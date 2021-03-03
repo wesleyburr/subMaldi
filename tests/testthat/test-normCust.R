0f50bc2f45e6d4ea7bcf3410b471ebde45784a34
Sophie Castel <sophiecastel@trentu.ca>
2021-03-03 15:55:45 -0500

    DEPRECATED: spec1 = , spec2 = , spec3 =---> spectra_cols

--- tests/testthat/test-normCust.R
+++ tests/testthat/test-normCust.R
@@ -11,14 +11,14 @@ library(subMALDI)
 test_that("No norm_mz selected when normMethod_custom yields error", {
   expect_error(normSpectra(
     dat = Master2, mass_dat = "full_mz", method = "custom",
-    spec1 = "Before1"
+    spectra_cols = "Before1"
   ))
 })
 
 test_that("Intensity of output at norm_mz == 1 == max", {
   out <- normSpectra(Master2, "full_mz",
     method = "custom",
-    norm_mz = 255.23, spec1 = "Before1", showHI = FALSE
+    norm_mz = 255.23, spectra_cols = "Before1", showHI = FALSE
   )
   max <- max(out$Before1)
 
@@ -36,7 +36,7 @@ test_that("Intensity of output at norm_mz == 1", {
 
   out <- normSpectra(Master, "full_mz",
     method = "custom_imprecise",
-    norm_mz = "112.959", spec1 = "Before1"
+    norm_mz = "112.959", spectra_cols = "Before1"
   )
   int <- out[which(out$full_mz == 112.9595), 2]
 
@@ -48,12 +48,12 @@ test_that("More than one peak for given m/z", {
 
   out <- normSpectra(Master, "full_mz",
   method = "custom_imprecise",
-    norm_mz = "112.959", spec1 = "Before1"
+    norm_mz = "112.959", spectra_cols = "Before1"
   )
   int <- out[which(out$full_mz == 112.9595), 2]
 
   expect_error(normSpectra(Master, "full_mz",
     method = "custom_imprecise",
-    norm_mz = "255.23", spec1 = "Before1"
+    norm_mz = "255.23", spectra_cols = "Before1"
   ))
 })
